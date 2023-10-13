38c6124635f2334dc72884dda1f9d75046afdf4a
Daniel <zrelick@gmail.com>
2023-10-13 14:35:12 +0200

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 //absolute path
 import './../styles/globals.css';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics /></body>
     </html>
   );
 }
